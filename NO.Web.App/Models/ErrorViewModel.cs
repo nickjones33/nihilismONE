15fd3c69cfc75897fbfd43e90b52f51d9875985b
Nicholas Jones <nicholasaaronjones@gmail.com>
2019-04-09 16:44:37 -0500

    init

---
+++ NO.Web.App/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NO.Web.App.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
