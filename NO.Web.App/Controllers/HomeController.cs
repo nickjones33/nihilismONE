04dcee672c9ed92a0c400cd9633c3c2c5df3abcb
Nicholas Jones <nicholasaaronjones@gmail.com>
2019-04-09 17:39:22 -0500

    adding login

--- NO.Web.App/Controllers/HomeController.cs
+++ NO.Web.App/Controllers/HomeController.cs
@@ -6,24 +6,23 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using NO.Web.App.Models;
 
-namespace NO.Web.App.Controllers
-{
-    public class HomeController : Controller
-    {
-        public IActionResult Index()
-        {
-            return View();
+namespace NO.Web.App.Controllers {
+    public class HomeController : Controller {
+        public IActionResult Index () {
+            return View ();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
+        public IActionResult Login () {
+            return View ();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        public IActionResult Privacy () { //fuck off
+            return View ();
+        }
+
+        [ResponseCache (Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error () {
+            return View (new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
